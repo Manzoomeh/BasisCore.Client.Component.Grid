1cc06955837238b8bc7c5ed8a198a792aaa2eebf
1senka <58356827+1senka@users.noreply.github.com>
2024-05-14 10:12:00 +0330

    bug fix on paste

--- src/component/grid/Grid.ts
+++ src/component/grid/Grid.ts
@@ -177,18 +177,21 @@ export default class Grid implements IGrid {
       this.performAction(input);
     }
   }
+
   private handleRowInput(
     input: HTMLInputElement,
     columnInfo: IGridColumnInfo,
     event?: KeyboardEvent
   ) {
-    const newFilter = input.value.toLowerCase();
+    console.log("here");
     clearTimeout(this.timerId);
     if (
       this.options.process === "server" &&
       !(event?.key === "Enter" || event?.keyCode === 13)
     ) {
       this.timerId = setTimeout(() => {
+        let newFilter = input.value.toLowerCase();
+
         let mustUpdate = false;
         if (newFilter.length > 0) {
           if (!this.processManager.filter) {
@@ -212,9 +215,10 @@ export default class Grid implements IGrid {
         }
       }, 1000);
     } else {
-      input.addEventListener("keyup", (_) => {
+      console.log("heree");
+      this.timerId = setTimeout(() => {
         const newFilter = input.value.toLowerCase();
-
+        console.log("object :>> ", newFilter);
         let mustUpdate = false;
         if (newFilter.length > 0) {
           if (!this.processManager.filter) {
@@ -236,7 +240,7 @@ export default class Grid implements IGrid {
           this.processManager.pageNumber = 0;
           this.processManager.applyUserAction();
         }
-      });
+      }, 5);
     }
   }
 
